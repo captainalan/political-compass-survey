b6f96db7526d9786b8a25282e60df15f88093f7e
Alan Wong <captainalan@gmail.com>
2019-06-19 18:51:28 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const mustacheExpress = require("mustache-express");
+const app = express();
+
+// Register .mustache extension with The Mustache Express
+app.engine('mustache', mustacheExpress());
+
+app.set("view engine", "mustache");
+app.set("views", __dirname + "/app/views");
+
+// Serve static assets
+app.use(express.static(__dirname + "/app/public"));
+
+app.get("/", (req, res) => {
+    res.render("home");
+});
+
+app.get("/survey", (req, res) => {
+    res.render("survey");
+});
+
+app.get("/layout", (req, res) => {
+    res.render("layout");
+});
+
+app.listen(3000, () => {
+    console.log("Your app is running on port 3000");
+});
